2544c0fcd1e001d44dfaceeedd0bc24130abfc5b
harshad00 <bholo0090@gmail.com>
2025-06-04 18:23:39 +0530

    DSA start

---
+++ basic-cpp/bs2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  
+    int age = 23;
+    double pi = 3.14;
+    char grade = 'A';
+    string name = "Harshad";
+    bool isHappy = true;
+
+    cout << "Name:" << name <<", Age:" << age << endl;
+    cout << "PI:" << pi << ", Grade:" << grade << endl;
+    return 0;
+
+}
---
+++ basic-cpp/loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int age  = 23;
+
+if (age >= 18) {
+    cout << "Adult";
+} else {
+    cout << "Minor";
+}
+}
