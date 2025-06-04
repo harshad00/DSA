5ce11d74ee94ed195748c3a0bf5081d27c94c18f
harshad00 <bholo0090@gmail.com>
2025-06-04 19:11:44 +0530

    Patterns

---
+++ basic-Js/bs1.js
@@ -0,0 +1,24 @@
+// console.log('Hello world');
+
+// for (let i = 0; i < 5; i++) {
+//     let row = ''
+//     for (let j = 0; j < 5; j++) {
+//         row += '*'
+//     }
+//     console.log(row);
+// }
+
+
+// for (let i = 0; i < 5; i++){
+//     let row = '';
+//     for (let j = 5; j > i ; j--){
+//         row += '*';
+//     }
+//     console.log(row);
+    
+// }
+
+
+
+
+
