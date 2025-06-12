9f74cad8bfa0ab927e14ba4efede422a8dd5c2b2
harshad00 <bholo0090@gmail.com>
2025-06-12 18:31:15 +0530

    s=String mothods

--- basic-Js/bs1.js
+++ basic-Js/bs1.js
@@ -19,6 +19,53 @@
 // }
 
 
+//! 1. Print the length of "JavaScript"
+// let str = "Javascript"
+// console.log(str.length); //? 10
 
+// ! 2. Find the first and last index of 'a' in "banana"
+// let str = "banana"
+// console.log('first ', str.indexOf('a')); //? 1
+// console.log('last ', str.lastIndexOf('a')); //? 5
+ 
+ 
+// !3. Convert "I love Js" to uppercase.
+
+// console.log("I love JS".toUpperCase());  //? "I LOVE JS"
+
+// !4. Check if "frontend" includes "end".
+//  console.log("frontend".includes("end")); //? true
+
+//  !5. Trim the string " spaced out ".
+
+// console.log(" spaced out ".trim());//? "spaced out".
+
+// !6. Extract "code" from "decode" using slice();
+
+// console.log("decode".slice(2));//? code
+
+//! 7. Replace "bad" with "good" in "bad habit".
+
+// console.log("bad habit".replace("bad","good")); //? "good habit"
+
+// !8. Split "name,age,city" into an array
+// console.log("name,age,city".split(",")); //? ["name", "age", "city" ]
+
+// !9. Repeat the string "Hi " 3 items.
+ 
+// console.log("Hi ".repeat(3));//? Hi Hi Hi
+
+// !10. Check if "Hello" starts with "He".
+console.log("Hello".startsWith("He")); //? True
+ 
+
+
+
+
+
+
+ 
+ 
+ 
 
 
